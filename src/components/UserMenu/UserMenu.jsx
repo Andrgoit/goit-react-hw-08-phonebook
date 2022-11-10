1d093cb5d5a797b1470ec06ec8477fdbc017aebf
Andrgoit <stakgrom@gmail.com>
2022-11-10 16:44:27 +0200

    remove userMenu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -6,16 +6,16 @@ import { userName, userEmail } from 'redux/auth/auth-selectors';
 import { logout } from 'redux/auth/auth-operations';
 
 export default function UserMenu() {
-  const user = useSelector(userName);
-  const email = useSelector(userEmail);
+  // const user = useSelector(userName);
+  // const email = useSelector(userEmail);
   const dispatch = useDispatch();
 
   return (
     <Box display="flex" alignItems="center">
       <StyledLink to="/contacts">Contacts</StyledLink>
-      <StyledUserText>
+      {/* <StyledUserText>
         Hi, {user} ({email})
-      </StyledUserText>
+      </StyledUserText> */}
       <StyledBtn onClick={() => dispatch(logout())}>Logout</StyledBtn>
     </Box>
   );
